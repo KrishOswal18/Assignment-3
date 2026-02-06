bea7b7f4e8d6bc5291cbdf639d5089f4d8d3f4b8
KrishOswal18 <krishoswal1811@gmail.com>
2026-02-06 14:34:12 +0530

    Ass3

---
+++ task2.py
@@ -0,0 +1,12 @@
+import math
+
+num = int(input("Enter a num: "))
+
+sq = math.sqrt(num)
+print(f"Square root: {sq}")
+
+log = math.log(num)
+print(f"Logarithm: {log}")
+
+sin = math.sin(num)
+print(f"Sine: {sin}")
